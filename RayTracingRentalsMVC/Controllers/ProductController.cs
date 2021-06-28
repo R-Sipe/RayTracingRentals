ab73270de1a6ce2d16761ca8c87041131188161a
Raymond Sipes <raymondsipes21@gmail.com>
2021-06-28 10:38:58 -0400

    Added product list item in models, added product controller nd index view for product,

---
+++ RayTracingRentalsMVC/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using RayTracingRentals.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RayTracingRentalsMVC.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var model = new ProductListItem[0];
+            return View(model);
+        }
+    }
+}
