a73c86009730b614e2390ba628b33d5de8fe0d91
Raymond Sipes <raymondsipes21@gmail.com>
2021-06-28 16:52:53 -0400

    added rental order data layer

---
+++ RayTracingRental.Data/RentalOrder.cs
@@ -0,0 +1,26 @@
+﻿using RayTracingRentals.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracingRental.Data
+{
+    public class RentalOrder
+    {
+        [Key]
+        public int RentalOrderId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Returned { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int CustomerId { get; set; }
+        public virtual List<Customer> Customer { get; set; }
+        public int ProductId { get; set; }
+        public virtual List<Product> Product { get; set; }
+    }
+}
