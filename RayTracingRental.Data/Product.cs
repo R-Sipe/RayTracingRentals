e9e227889f7c841ff9df5580d9fbdd98a0f70448
Raymond Sipes <raymondsipes21@gmail.com>
2021-06-28 10:14:19 -0400

    Added info for identity models and added product data layer

---
+++ RayTracingRental.Data/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracingRental.Data
+{
+    public enum Console { PlayStation=1, Xbox, Nitendo, PC }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public decimal Price { get; set; }
+        
+        [Required]
+        public bool FamilyFriendly { get; set; }
+        
+        [Required]
+        public Console Console { get; set; }
+    }
+}
